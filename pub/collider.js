74d9551ebe4ac94254472b27f26185f02d0e4f1e
Igor Khotin <khotin@gmx.com>
2020-05-31 22:00:37 +0300

    [+] gif files autoloading

--- pub/collider.js
+++ pub/collider.js
@@ -3147,7 +3147,7 @@ Mod.prototype.batchLoad = function(batch, url, base, path) {
     _.log.sys('loader-' + batch, '.' + ext + ': ' + url + ' -> ' + addPath(base.name, path))
 
     switch (ext) {
-        case 'png': case 'jpge': case 'jpg': case 'svg':
+        case 'png': case 'jpeg': case 'jpg': case 'svg': case 'gif':
             patchImg(_, batch, url, base, path, classifier, onLoad)
             break
 
