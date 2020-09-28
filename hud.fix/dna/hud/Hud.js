2a11c570bdd4efd57a2bd67abb7ea5b7a12d11a9
Igor Khotin <khotin@gmx.com>
2020-09-28 23:17:09 +0300

    [*] help for hud

--- hud.fix/dna/hud/Hud.js
+++ hud.fix/dna/hud/Hud.js
@@ -5,9 +5,9 @@
 //
 // @depends(/dna/hud/Container)
 let instances = 0
-let Hud = function(dat) {
+let Hud = function(st) {
     this.name = 'hud' + ++instances
-    dna.hud.Container.call(this, dat)
+    dna.hud.Container.call(this, st)
 
     this.span = true
     this.transparent = true
@@ -20,7 +20,7 @@ let Hud = function(dat) {
     this.w = ctx.width
     this.h = ctx.height
 
-    //sys.augment(this, dat)
+    //sys.augment(this, st)
     
     // fix preset if not present
     //env.attach(sys.clone(dna.hud.preset), 'hud')
@@ -123,34 +123,90 @@ Hud.prototype.style = function(path, source) {
     return lib.hud.style(path, source)
 }
 
+// a service call to save the size to use by isResized() later
 Hud.prototype.saveSize = function() {
     this._w = this.w
     this._h = this.h
 }
 
+// a service call to test if the hud has been resized
 Hud.prototype.isResized = function() {
     return (this._w !== this.w || this._h !== this.h)
 }
 
+// called when hud needs to update it's position and size
 Hud.prototype.adjust = function() {
     Container.prototype.adjust.call(this)
     this.saveSize()
-}
 
+
+// translate global x to local coordinates
+// @param {number} x - global x
+// @returns {number} - local x
 Hud.prototype.lx = function(x) {
     return x - this.x
 }
 
+// translate global y to local coordinates
+// @param {number} y - global y
+// @returns {number} - local y
 Hud.prototype.ly = function(y) {
     return y - this.y
 }
 
+// translate x,y to local coordinate system
+// @param {number} x - global x
+// @param {number} y - global y
+// @returns {object/2d-vector} - {x,y} in local coordinates
+Hud.prototype.lxy = function(x, y) {
+    return {
+        x: x - this.x,
+        y: y - this.y,
+    }
+}
+
+// translate local x to global coordinates
+// @param {number} x - local x
+// @returns {number} - global x
+Hud.prototype.gx = function(x) {
+    return x + this.x
+}
+
+// translate local y to global coordinates
+// @param {number} y - local y
+// @returns {number} - global y
+Hud.prototype.gy = function(y) {
+    return y + this.y
+}
+
+// translate x,y to parent node coordinate system
+// @param {number} x - local x
+// @param {number} y - local y
+// @returns {object/2d-vector} - {x,y} in parent node coordinates
+Hud.prototype.gxy = function(x, y) {
+    return {
+        x: x + this.x,
+        y: y + this.y,
+    }
+}
 
 
+// handles mouse down event
+// @param {number} x
+// @param {number} y
+// @param {number} b - buttons mask form the mouse event
+// @param {object} e - original mouse event
+// @returns {boolean} - true if focus is captured
 Hud.prototype.onMouseDown = function(x, y, b, e) {
     return Container.prototype.onMouseDown.call(this, x, y, b, e)
 }
 
+// handle mouse up event
+// @param {number} x
+// @param {number} y
+// @param {number} b - buttons mask form the mouse event
+// @param {object} e - original mouse event
+// @returns {boolean} - true if focus is captured
 Hud.prototype.onMouseUp = function(x, y, b, e) {
     this.captured.forEach(g => {
         if (sys.isFun(g.onMouseUp)) g.onMouseUp(x, y, e)
@@ -159,6 +215,10 @@ Hud.prototype.onMouseUp = function(x, y, b, e) {
     this.releaseMouse()
 }
 
+// handle mouse move event
+// @param {number} x
+// @param {number} y
+// @param {object} e - original mouse event
 Hud.prototype.onMouseMove = function(x, y, e) {
     const dx = _._$.env.mouse.x - _._$.env.mouse.lx
     const dy = _._$.env.mouse.y - _._$.env.mouse.ly
@@ -178,23 +238,30 @@ Hud.prototype.onTouchStart = function(x, y, e) {
 }
 */
 
+// a service call to handle touch end events 
+// @param {object} e - an original touch event object
 Hud.prototype.onTouchEnd = function(e) {
     this.releaseTouch(e)
 }
 
 
+// a service call to handle key down events
+// @param {object} e - an original key event
 Hud.prototype.onKeyDown = function(e) {
     this.focused.forEach(g => {
         if (sys.isFun(g.onKeyDown)) g.onKeyDown(e)
     })
 }
 
+// a service call to handle key up events
+// @param {object} e - an original key event
 Hud.prototype.onKeyUp = function(e) {
     this.focused.forEach(g => {
         if (sys.isFun(g.onKeyUp)) g.onKeyUp(e)
     })
 }
 
+// a service call to expand the hud to the whole screen
 Hud.prototype.expand = function() { // calculate operating area
     this.x = 0
     this.y = 0
@@ -202,11 +269,15 @@ Hud.prototype.expand = function() { // calculate operating area
     this.h = ctx.height
 }
 
+// a service call tracking gadget mouse capture
+// Includes a gadget into the captured list.
+// @param {object} gadget
 Hud.prototype.captureMouse = function(gadget) {
     gadget._captured = true
     if (this.captured.indexOf(gadget) < 0) this.captured.push(gadget)
 }
 
+// a service call to clear the captured gadgets list
 Hud.prototype.releaseMouse = function() {
     this.captured.forEach(g => {
         g._captured = false
@@ -214,11 +285,15 @@ Hud.prototype.releaseMouse = function() {
     this.captured = []
 }
 
+// a service call for tracking touched gadgets
+// Includes a gadget into the captured list.
+// @param {object} gadget
 Hud.prototype.captureTouch = function(gadget) {
     gadget._touched = true
     if (this.touched.indexOf(gadget) < 0) this.touched.push(gadget)
 }
 
+// a service call to clear the touched gadgets list
 Hud.prototype.releaseTouch = function(e) {
     this.touched.forEach(g => {
         g._touched = false
@@ -227,6 +302,9 @@ Hud.prototype.releaseTouch = function(e) {
     this.touched = []
 }
 
+// a service call to track focused gadgets
+// Includes a gadget into the focused list
+// @param {object/gadget}
 Hud.prototype.captureFocus = function(gadget) {
     if (this.focused.indexOf(gadget) < 0) {
         gadget.focus = true
@@ -235,6 +313,8 @@ Hud.prototype.captureFocus = function(gadget) {
     }
 }
 
+// a service call to clear the focused gadgets list
+// @param {object/gadget}
 Hud.prototype.releaseFocus = function(gadget) {
     if (!gadget) {
         this.focused = []
@@ -255,6 +335,7 @@ Hud.prototype.releaseFocus = function(gadget) {
     }
 }
 
+// draw the hud
 Hud.prototype.draw = function() {
     if (this.span) this.expand()
     if (this.isResized()) this.adjust()
