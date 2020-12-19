481a1f869958c6cd2f397da76daa8687e6a69c88
Igor Khotin <khotin@gmx.com>
2020-12-19 23:59:07 +0200

    [+] lib.array.prev() element

--- lib.fix/lib/array.js
+++ lib.fix/lib/array.js
@@ -1,3 +1,8 @@
+// select the next element from the array
+// @param {array} ls - the source array
+// @param {object} cur - current element
+// @param {function} predicate - an optional filter
+// @returns {object} - the selected element, undefined if nothing found
 function next(ls, cur, predicate) {
     let i = 0
     if (cur) i = ls.indexOf(cur) + 1
@@ -28,6 +33,46 @@ function next(ls, cur, predicate) {
                 }
             }
         } while (!next && i < start)
+    }
+    return next
+}
+
+// select the previous element from the array
+// @param {array} ls - the source array
+// @param {object} cur - current element
+// @param {function} predicate - an optional filter
+// @returns {object} - the selected element, undefined if nothing found
+function prev(ls, cur, predicate) {
+    let i = ls.length - 1
+    if (cur) i = ls.indexOf(cur) - 1
+    if (i < 0) i = ls.length - 1
+    const start = i
+
+    let next
+    do {
+        const e = ls[i--]
+        if (e) {
+            if (isFun(predicate)) {
+                if (predicate(e)) next = e
+            } else {
+                next = e
+            }
+        }
+    } while (!next && i >= 0)
+
+    if (!next) {
+        // haven't found before cur, try from the end
+        i = ls.length - 1
+        do {
+            const e = ls[i--]
+            if (e) {
+                if (isFun(predicate)) {
+                    if (predicate(e)) next = e
+                } else {
+                    next = e
+                }
+            }
+        } while (!next && i > start)
     }
     return next
 }
