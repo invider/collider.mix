6115c2807b022444b432674ca4ca251b0ac7c48b
Igor Khotin <khotin@gmx.com>
2021-02-13 00:39:26 +0200

    [-] fix SlideCamera viewport test

--- ext.fix/dna/SlideCamera.js
+++ ext.fix/dna/SlideCamera.js
@@ -376,7 +376,7 @@ SlideCamera.prototype.draw = function(dt) {
     this._ls.forEach( e => {
         if (e.draw && !e.dead && !e.hidden) {
             // culling
-            if (e._sizable) {
+            if (e._rectangular) {
                 if ((e._centered
                             && e.x+e.w/2 >= vp[0]
                             && e.x-e.w/2 <= vp[2]
