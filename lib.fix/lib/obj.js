52dca36c079a16def0acde0b693e93ad1b5cfcb6
Igor Khotin <khotin@gmx.com>
2021-03-27 23:23:24 +0200

    [+] obj.toData()

---
+++ lib.fix/lib/obj.js
@@ -0,0 +1,15 @@
+// transform an entity to data object
+// Useful for persistence.
+// @param {object} entity - source entity object
+// @param {array/string} exclude - list of entities to exclude
+// @returns {object} - data object without functions and excluded keys
+function toData(entity, exclude) {
+    const data = {}
+    Object.keys(entity).forEach(k => {
+        if (exclude && exclude.indexOf(k) >= 0) return // skipping
+        const o = entity[k]
+        if (isFun(o)) return
+        data[k] = o
+    })
+    return data
+}
