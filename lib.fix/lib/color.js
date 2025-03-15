7840cc745cf5705509eb6e56a8a50ed709211bf8
Igor Khotin <khotin@gmx.com>
2025-03-15 23:59:17 +0200

    [*] move color shifting routines out of global scope to color object

--- lib.fix/lib/color.js
+++ lib.fix/lib/color.js
@@ -7,3 +7,113 @@ const blue   = '#0000ff'
 const yellow = '#ffff00'
 const cyan   = '#00ffff'
 const pink   = '#ff00ff'
+
+function color2RGBA(c) {
+    if (c.startsWith('#')) c = c.substring(1)
+    const R = parseInt(c.substring(0, 2), 16)
+    const G = parseInt(c.substring(2, 4), 16)
+    const B = parseInt(c.substring(4, 6), 16)
+    let A = 255
+    if (c.length > 6) A = parseInt(c.substring(6, 8), 16)
+    return [R, G, B, A]
+}
+
+function color2rgba(c) {
+    if (c.startsWith('#')) c = c.substring(1)
+    const R = parseInt(c.substring(0, 2), 16)
+    const G = parseInt(c.substring(2, 4), 16)
+    const B = parseInt(c.substring(4, 6), 16)
+    let A = 255
+    if (c.length > 6) A = parseInt(c.substring(6, 8), 16)
+    return [R/255, G/255, B/255, A/255]
+}
+
+function rgb2hsl(r, g, b) {
+    // find greatest and smallest channel values
+    const cmin = Math.min(r,g,b)
+    const cmax = Math.max(r,g,b)
+    const delta = cmax - cmin
+    let h = 0, s = 0, l = 0
+
+    // hue
+    if (delta == 0) h = 0;
+    else if (cmax == r) h = ((g - b) / delta) % 6
+    else if (cmax == g) h = (b - r) / delta + 2
+    else h = (r - g) / delta + 4;
+    //h = Math.round(h * 60);
+    h = h / 6
+    if (h < 0) h += 1
+
+    // lightness
+    l = (cmax + cmin) / 2
+
+    // saturation
+    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1))
+
+    // multiply l and s by 100
+    s = Math.min(Math.abs(s), 1)
+    l = Math.min(Math.abs(l), 1)
+
+    //s = Math.min(+(s * 100).toFixed(1), 100)
+    //l = Math.min(+(l * 100).toFixed(1), 100)
+
+    //return "hsl(" + h + "," + s + "%," + l + "%)"
+    return [h, s, l]
+}
+
+function RGB2HSL(r, g, b) {
+    r /= 255
+    g /= 255
+    b /= 255
+
+    // find greatest and smallest channel values
+    const cmin = Math.min(r,g,b)
+    const cmax = Math.max(r,g,b)
+    const delta = cmax - cmin
+    let h = 0, s = 0, l = 0
+
+    // hue
+    if (delta == 0) h = 0;
+    else if (cmax == r) h = ((g - b) / delta) % 6
+    else if (cmax == g) h = (b - r) / delta + 2
+    else h = (r - g) / delta + 4;
+    h = Math.round(h * 60);
+
+    // make negative hues positive behind 360°
+    if (h < 0) h += 360
+
+    // lightness
+    l = (cmax + cmin) / 2
+
+    // saturation
+    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1))
+
+    // multiply l and s by 100
+    s = Math.min(+(s * 100).toFixed(1), 100)
+    l = Math.min(+(l * 100).toFixed(1), 100)
+
+    //return "hsl(" + h + "," + s + "%," + l + "%)"
+    return [h, s, l]
+}
+
+
+function lighten(c, factor) {
+    const crgba = color2rgba(c)
+    const chsl = rgb2hsl(crgba[0], crgba[1], crgba[2])
+    chsl[2] = clamp(chsl[2] * factor, 0, 1)
+    return hsl(chsl[0], chsl[1], chsl[2])
+}
+
+function saturate(c, factor) {
+    const crgba = color2rgba(c)
+    const chsl = rgb2hsl(crgba[0], crgba[1], crgba[2])
+    chsl[1] = clamp(chsl[1] * factor, 0, 1)
+    return hsl(chsl[0], chsl[1], chsl[2])
+}
+
+function shiftHue(c, shift) {
+    const crgba = color2rgba(c)
+    const chsl = rgb2hsl(crgba[0], crgba[1], crgba[2])
+    chsl[0] = Math.abs((chsl[0] + shift) % 1)
+    return hsl(chsl[0], chsl[1], chsl[2])
+}
--- pub/collider.js
+++ pub/collider.js
@@ -378,94 +378,6 @@ function hue2rgb(p, q, t) {
     return p
 }
 
-function color2RGBA(c) {
-    if (c.startsWith('#')) c = c.substring(1)
-    const R = parseInt(c.substring(0, 2), 16)
-    const G = parseInt(c.substring(2, 4), 16)
-    const B = parseInt(c.substring(4, 6), 16)
-    let A = 255
-    if (c.length > 6) A = parseInt(c.substring(6, 8), 16)
-    return [R, G, B, A]
-}
-
-function color2rgba(c) {
-    if (c.startsWith('#')) c = c.substring(1)
-    const R = parseInt(c.substring(0, 2), 16)
-    const G = parseInt(c.substring(2, 4), 16)
-    const B = parseInt(c.substring(4, 6), 16)
-    let A = 255
-    if (c.length > 6) A = parseInt(c.substring(6, 8), 16)
-    return [R/255, G/255, B/255, A/255]
-}
-
-function rgb2hsl(r, g, b) {
-    // find greatest and smallest channel values
-    const cmin = Math.min(r,g,b)
-    const cmax = Math.max(r,g,b)
-    const delta = cmax - cmin
-    let h = 0, s = 0, l = 0
-
-    // hue
-    if (delta == 0) h = 0;
-    else if (cmax == r) h = ((g - b) / delta) % 6
-    else if (cmax == g) h = (b - r) / delta + 2
-    else h = (r - g) / delta + 4;
-    //h = Math.round(h * 60);
-    h = h / 6
-    if (h < 0) h += 1
-
-    // lightness
-    l = (cmax + cmin) / 2
-
-    // saturation
-    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1))
-
-    // multiply l and s by 100
-    s = Math.min(Math.abs(s), 1)
-    l = Math.min(Math.abs(l), 1)
-
-    //s = Math.min(+(s * 100).toFixed(1), 100)
-    //l = Math.min(+(l * 100).toFixed(1), 100)
-
-    //return "hsl(" + h + "," + s + "%," + l + "%)"
-    return [h, s, l]
-}
-
-function RGB2HSL(r, g, b) {
-    r /= 255
-    g /= 255
-    b /= 255
-
-    // find greatest and smallest channel values
-    const cmin = Math.min(r,g,b)
-    const cmax = Math.max(r,g,b)
-    const delta = cmax - cmin
-    let h = 0, s = 0, l = 0
-
-    // hue
-    if (delta == 0) h = 0;
-    else if (cmax == r) h = ((g - b) / delta) % 6
-    else if (cmax == g) h = (b - r) / delta + 2
-    else h = (r - g) / delta + 4;
-    h = Math.round(h * 60);
-
-    // make negative hues positive behind 360°
-    if (h < 0) h += 360
-
-    // lightness
-    l = (cmax + cmin) / 2
-
-    // saturation
-    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1))
-
-    // multiply l and s by 100
-    s = Math.min(+(s * 100).toFixed(1), 100)
-    l = Math.min(+(l * 100).toFixed(1), 100)
-
-    //return "hsl(" + h + "," + s + "%," + l + "%)"
-    return [h, s, l]
-}
-
 function rgb(r, g, b) {
     r = clamp(Math.round(r * 255), 0, 255).toString(16)
     g = clamp(Math.round(g * 255), 0, 255).toString(16)
@@ -521,12 +433,13 @@ function hsl(h, s, l) {
     if (s === 0) {
         return rgb(l, l, l)
     } 
-    const q = l < 0.5? l*(1+s) : l + s - l*s
-    const p = 2*l - q
-    const r = hue2rgb(p, q, h + 1/3)
-    const g = hue2rgb(p, q, h)
-    const b = hue2rgb(p, q, h - 1/3)
-    return rgb(r, g, b)
+    const q = l < 0.5? l*(1+s) : l + s - l*s,
+          p = 2*l - q
+    return rgb(
+          hue2rgb(p, q, h + 1/3),
+          hue2rgb(p, q, h),
+          hue2rgb(p, q, h - 1/3)
+    )
 }
 
 function hsla(h, s, l, a) {
@@ -543,27 +456,6 @@ function hsla(h, s, l, a) {
     const g = hue2rgb(p, q, h)
     const b = hue2rgb(p, q, h - 1/3)
     return rgba(r, g, b, a)
-}
-
-function lighten(c, factor) {
-    const crgba = color2rgba(c)
-    const chsl = rgb2hsl(crgba[0], crgba[1], crgba[2])
-    chsl[2] = clamp(chsl[2] * factor, 0, 1)
-    return hsl(chsl[0], chsl[1], chsl[2])
-}
-
-function saturate(c, factor) {
-    const crgba = color2rgba(c)
-    const chsl = rgb2hsl(crgba[0], crgba[1], crgba[2])
-    chsl[1] = clamp(chsl[1] * factor, 0, 1)
-    return hsl(chsl[0], chsl[1], chsl[2])
-}
-
-function shiftHue(c, shift) {
-    const crgba = color2rgba(c)
-    const chsl = rgb2hsl(crgba[0], crgba[1], crgba[2])
-    chsl[0] = Math.abs((chsl[0] + shift) % 1)
-    return hsl(chsl[0], chsl[1], chsl[2])
 }
 
 // TODO make into regular obj.fn = before(patch, obj.fn)
@@ -3018,13 +2910,6 @@ function augmentCtx(ctx) {
         RGBA: RGBA,
         hsl: hsl,
         hsla: hsla,
-        RGB2HSL: RGB2HSL,
-        rgb2hsl: rgb2hsl,
-        color2RGBA: color2RGBA,
-        color2rgba: color2rgba,
-        lighten: lighten,
-        saturate: saturate,
-        shiftHue: shiftHue,
     }
     return ctx
 }
