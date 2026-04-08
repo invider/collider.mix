868cb07102521d43121904a49d962375f0a9cd19
Igor Khotin <khotin@gmx.com>
2026-04-08 23:59:48 +0300

    [*] update kinetix NG

--- lib.fix/dna/kinetix/KinetixNG.js
+++ lib.fix/dna/kinetix/KinetixNG.js
@@ -13,15 +13,25 @@ class KinetixNG {
     key(mapFn, easing) {
         // TODO resurrect a zombie if possible
         const k = {
+            // initial state
             at:     env.time,
-            freq:   1,
-            times:  1,
+
+            // === key setup ===
             easing: easing,
             mapFn:  mapFn,
-
+            freq:   1,
+            steps:  1,
+            // flags setup
             mirror: false,
             loop:   false,
             dead:   false,
+            // event handlers setup
+            onStep: null,
+            onKill: null,
+
+            // key state
+            dead:   false,
+            mark:   0,
         }
 
         this.keys.push(k)
@@ -31,6 +41,11 @@ class KinetixNG {
 
     freq(fq) {
         this.last.freq = fq
+        return this
+    }
+
+    steps(n) {
+        this.last.steps = n
         return this
     }
 
@@ -44,8 +59,13 @@ class KinetixNG {
         return this
     }
 
-    done(doneFn) {
-        this.last.done = doneFn
+    onStep(stepFn) {
+        this.last.onStep = stepFn
+        return this
+    }
+
+    onKill(killFn) {
+        this.last.onKill = killFn
         return this
     }
 
@@ -59,25 +79,34 @@ class KinetixNG {
         for (let i = 0; i < N; i++) {
             const key = keys[i]
             if (!key.dead) {
-                const t = (env.time - key.at) * key.freq
-                if (t > key.times && !key.loop) {
-                    key.mapFn( key.easing(1), 1 )
-                    key.dead = true
-                    if (key.done) key.done()
-                } else {
-                    if (key.mirror) {
-                        const tt = t % 2
-                        if (tt >= 1) {
-                            const ttt = 1 - (tt - 1)
-                            key.mapFn( key.easing(ttt), ttt )
-                        } else {
-                            const ttt = t % 1
-                            key.mapFn( key.easing(ttt), ttt )
-                        }
-                    } else {
-                        const tt = t % 1
-                        key.mapFn( key.easing(tt), tt )
+                const t = (env.time - key.at) * key.freq,  // time in easing scale [0..1...]
+                      T = t | 0  // full steps
+
+                if (t - key.mark > 1) {
+                    key.mark = T
+                    if (key.onStep) key.onStep()
+                    // TODO what to do with the exact 1, 2, 3 hits? should we map them?
+
+                    if (!key.loop && T >= key.steps) {
+                        key.mapFn( key.easing(1), T )
+                        key.dead = true
+                        if (key.onKill) key.onKill()
+                        continue
                     }
+                }
+
+                if (key.mirror) {
+                    const tt = t % 2
+                    if (tt >= 1) {
+                        const ttt = 1 - (tt - 1)
+                        key.mapFn( key.easing(ttt), t )
+                    } else {
+                        const ttt = t % 1
+                        key.mapFn( key.easing(ttt), t )
+                    }
+                } else {
+                    const tt = t % 1
+                    key.mapFn( key.easing(tt), t )
                 }
             }
         }
