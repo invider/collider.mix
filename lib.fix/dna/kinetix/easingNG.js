89ca00216afba4c35a5509a85b5155a5458e9154
Igor Khotin <khotin@gmx.com>
2026-04-12 23:24:16 +0300

    [+] kinetix quart, quint and sine easing functions

--- lib.fix/dna/kinetix/easingNG.js
+++ lib.fix/dna/kinetix/easingNG.js
@@ -2,15 +2,44 @@ const easingNG = {
 
     linear: t => t,
 
-    easeIn: t => t*t,
+    easeIn: t => t * t,
 
-    easeOut: t => Math.pow(1-t, 2),
+    easeOut: t => t * (2 - t),
 
-    easeInOut: t => (t <= .5)? .5 * Math.pow(2*t, 2) : .5 + .5 * ( 1 - Math.pow(1-(2*(t-.5)), 2) ),
+    //easeInOut: t => (t <= .5)? .5 * Math.pow(2*t, 2) : .5 + .5 * ( 1 - Math.pow( 1 - (2 * (t-.5)), 2 ) ),
+    easeInOut: t => (t < .5)? 2 * t * t : 1 - .5 * Math.pow(-2 * t + 2, 2),
 
-    easeInQubic: t => t*t*t,
+    easeInQubic: t => t * t * t,
 
-    easeOutQubic: t => Math.pow(1-t, 3),
+    easeOutQubic: t => 1 + (--t)*t*t,
+
+    easeInOutQubic: t => (t < .5)? .5 * Math.pow(2*t, 3) : .5 + .5 * ( 1 - Math.pow(1-(2*(t-.5)), 3) ),
+
+    easeInQuart: t => t * t * t * t,
+
+    easeOutQuart: t => 1 - (--t) * t * t * t,
+
+    easeInOutQuart: t => (t < .5)? 8 * t*t*t*t : 1 - 8 * (--t)*t*t*t,
+
+    easeInQuint: t => t * t * t * t * t,
+
+    easeOutQuint: t => 1 + (--t)*t*t*t*t,
+
+    easeInOutQuint: t => (t < .5)? 16 * t*t*t*t*t : 1 + 16 * (--t)*t*t*t*t,
+
+    easeInSine: t => -1 * Math.cos( t * (.5*Math.PI) ) + 1,
+
+    easeOutSine: t => Math.sin( t * (.5*Math.PI) ),
+
+    easeInOutSine: t => -.5 * ( Math.cos( Math.PI * t ) - 1 ),
+
+    easeInElastic: function(t) {
+        if (t === 0) return 0
+        if (t === 1) return 1
+        const T3 = TAU / 3
+
+        return (-Math.pow(2, 10 * t - 10) * Math.sin((t * 10 - 10.75) * T3))
+    },
 
     cubicBezier: function(t) {
         const p1x = this.p1x || .17,
