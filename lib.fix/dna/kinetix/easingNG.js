7ac1eb22ad3b0e892535f1fe27b08faf293488eb
Igor Khotin <khotin@gmx.com>
2026-04-13 22:38:41 +0300

    [*] restructure kinetix easing functions

--- lib.fix/dna/kinetix/easingNG.js
+++ lib.fix/dna/kinetix/easingNG.js
@@ -2,36 +2,47 @@ const easingNG = {
 
     linear: t => t,
 
-    easeIn: t => t * t,
+    quad: {
+        in: t => t * t,
 
-    easeOut: t => t * (2 - t),
+        out: t => t * (2 - t),
 
-    //easeInOut: t => (t <= .5)? .5 * Math.pow(2*t, 2) : .5 + .5 * ( 1 - Math.pow( 1 - (2 * (t-.5)), 2 ) ),
-    easeInOut: t => (t < .5)? 2 * t * t : 1 - .5 * Math.pow(-2 * t + 2, 2),
+        //easeInOut: t => (t <= .5)? .5 * Math.pow(2*t, 2) : .5 + .5 * ( 1 - Math.pow( 1 - (2 * (t-.5)), 2 ) ),
+        inOut: t => (t < .5)? 2 * t * t : 1 - .5 * Math.pow(-2 * t + 2, 2),
+    },
 
-    easeInQubic: t => t * t * t,
+    qubic: {
+        in: t => t * t * t,
 
-    easeOutQubic: t => 1 + (--t)*t*t,
+        out: t => 1 + (--t)*t*t,
 
-    easeInOutQubic: t => (t < .5)? .5 * Math.pow(2*t, 3) : .5 + .5 * ( 1 - Math.pow(1-(2*(t-.5)), 3) ),
+        inOut: t => (t < .5)? .5 * Math.pow(2*t, 3) : .5 + .5 * ( 1 - Math.pow(1-(2*(t-.5)), 3) ),
+    },
 
-    easeInQuart: t => t * t * t * t,
+    quart: {
+        in: t => t * t * t * t,
 
-    easeOutQuart: t => 1 - (--t) * t * t * t,
+        out: t => 1 - (--t) * t * t * t,
 
-    easeInOutQuart: t => (t < .5)? 8 * t*t*t*t : 1 - 8 * (--t)*t*t*t,
+        inOut: t => (t < .5)? 8 * t*t*t*t : 1 - 8 * (--t)*t*t*t,
+    },
 
-    easeInQuint: t => t * t * t * t * t,
+    quint: {
+        in: t => t * t * t * t * t,
 
-    easeOutQuint: t => 1 + (--t)*t*t*t*t,
+        out: t => 1 + (--t)*t*t*t*t,
 
-    easeInOutQuint: t => (t < .5)? 16 * t*t*t*t*t : 1 + 16 * (--t)*t*t*t*t,
+        inOut: t => (t < .5)? 16 * t*t*t*t*t : 1 + 16 * (--t)*t*t*t*t,
+    },
 
-    easeInSine: t => -1 * Math.cos( t * (.5*Math.PI) ) + 1,
 
-    easeOutSine: t => Math.sin( t * (.5*Math.PI) ),
+    sine: {
+        in: t => -1 * Math.cos( t * (.5*Math.PI) ) + 1,
 
-    easeInOutSine: t => -.5 * ( Math.cos( Math.PI * t ) - 1 ),
+        out: t => Math.sin( t * (.5*Math.PI) ),
+
+        inOut: t => -.5 * ( Math.cos( Math.PI * t ) - 1 ),
+    },
 
     easeInElastic: function(t) {
         if (t === 0) return 0
@@ -54,3 +65,36 @@ const easingNG = {
         )
     },
 }
+
+function catalog() {
+    const __ = easingNG
+    const ls  = []
+    const dir = {}
+
+    function capitalize(name) {
+        if (!name) return ''
+        return (name.substring(0, 1).toUpperCase() + name.substring(1))
+    }
+
+    function nextNode(node, name) {
+        if (isFun(node)) {
+            const fnName = name + capitalize(node.name)
+            node.alias = fnName
+            ls.push(node)
+            dir[fnName] = node
+        } else if (isObj(node)) {
+            Object.keys(node).forEach(key => {
+                const val = node[key]
+                const nextName = name + capitalize(node.name)
+                nextNode(val, nextName)
+            })
+        }
+    }
+    nextNode(__, 'ease')
+
+    __._ls  = ls
+    __._dir = dir
+}
+catalog()
+
+
